4ab3639cbfd58a7f3aa1bde5f8277d2c64579076
Kailai Li <kailai.li@kit.edu>
2020-11-12 13:20:56 +0100

    fixed typo

--- LiLi-OM-ROT/include/factors/LidarKeyframeFactor.h
+++ LiLi-OM-ROT/include/factors/LidarKeyframeFactor.h
@@ -42,7 +42,7 @@ struct LidarEdgeFactor
         Eigen::Matrix<T, 3, 1> lp;
         Eigen::Matrix<T, 3, 1> p_w;
         p_w = q_l_b.inverse() * (cp - t_l_b);
-        lp = q_last_curr * cp + t_last_curr;
+        lp = q_last_curr * p_w + t_last_curr;
 
         Eigen::Matrix<T, 3, 1> nu = (lp - lpa).cross(lp - lpb);
         Eigen::Matrix<T, 3, 1> de = lpa - lpb;
